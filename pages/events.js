6905af3eb8aa211922ae9c252d26889f912dec20
Antonia Tandler <q>
2023-06-30 03:15:19 +0200

    created events-page

---
+++ pages/events.js
@@ -0,0 +1,13 @@
+import Heading from "@/components/Heading";
+import Navigation from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function Events() {
+  return (
+    <>
+      <Heading />
+      <Navigation />
+      <Footer />
+    </>
+  );
+}
