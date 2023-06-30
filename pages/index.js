44a7aa015bdf6e4f6109a7c11e735862cff2a310
Antonia Tandler <q>
2023-06-30 03:12:20 +0200

    imported logo.jpg

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Heading from "@/components/Heading";
 import Navigation from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import styled from "styled-components";
+import Image from "next/image";
+
+const LogoImage = styled(Image)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  size: cover;
+  z-index: -2;
+`;
 
 export default function HomePage() {
   return (
     <>
       <Heading />
       <Navigation />
+      <LogoImage src="/../public/images/logo.jpg" width={600} height={500} />
       <Footer />
     </>
   );
