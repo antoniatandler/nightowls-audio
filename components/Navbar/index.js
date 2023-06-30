f7aac5c15b2e75c71d4d07eb6bba92b69590ea89
Antonia Tandler <q>
2023-06-30 03:17:22 +0200

    created and styled dj-dropdown in navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,57 +1,130 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
 
 const NavContainer = styled.div`
   background-color: rgb(255, 255, 255, 0.5);
-  color: white;
   position: fixed;
   top: 90px;
   width: 100%;
+  padding: 5px;
 `;
 
 const NavBar = styled.ul`
   color: black;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-evenly;
   margin: auto;
   padding: auto;
-  margin-left: 0;
-  padding-left: 0;
+  letter-spacing: 1px;
+  height: 30px;
 `;
 
 const NavItem = styled.li`
   list-style: none;
-  border-radius: 50px;
-  padding: 10px;
-  width: 180px;
-  text-align: center;
+  padding: 5px;
 `;
 
 const NavLink = styled(Link)`
   text-decoration: none;
   color: black;
+  padding-left: 5px;
+  padding-right: 5px;
+
+  &:hover,
+  &:focus {
+    color: rgb(139, 0, 0);
+    border-left: 5px solid rgb(139, 0, 0);
+    border-right: 5px solid rgb(139, 0, 0);
+    margin-left: -5px;
+    margin-right: -5px;
+    display: fixed;
+  }
+`;
+
+const SubNavItemList = styled.ul`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  z-index: 2;
+  padding: 0;
+`;
+
+const SubNavItem = styled.li`
+  list-style: none;
+  margin: 3px;
+  padding: 3px;
+  background-color: rgb(255, 255, 255, 0.5);
+  border-radius: 10px;
+  text-align: center;
+  letter-spacing: 1px;
+
+  &:hover,
+  &:focus {
+    color: rgb(139, 0, 0);
+    border-bottom: 3px solid rgb(139, 0, 0);
+    padding-bottom: 3px;
+  }
+`;
+
+const SubNavLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+
+  &:hover,
+  &:focus {
+    color: rgb(139, 0, 0);
+    border-bottom: 3px solid rgb(139, 0, 0);
+    padding-bottom: 3px;
+  }
 `;
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <NavContainer>
         <NavBar>
           <NavItem>
-            <NavLink href="/">Home</NavLink>
+            <NavLink href="/">HOME</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/booking">Bookings</NavLink>
+            <NavLink href="/booking" onMouseOver={toggleMenu}>
+              NIGHTOWLS AGENCY
+            </NavLink>
+            <SubNavItemList isOpen={isOpen} onMouseLeave={toggleMenu}>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+              <SubNavItem>
+                <SubNavLink href="/booking/">BJARNE</SubNavLink>
+              </SubNavItem>
+            </SubNavItemList>
           </NavItem>
           <NavItem>
-            <NavLink href="/rentail">Rentail</NavLink>
+            <NavLink href="/events">EVENTS</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/deco">Deco</NavLink>
+            <NavLink href="/rentail">RENTAIL & SERVICE </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/contact">Contact</NavLink>
+            <NavLink href="/contact">CONTACT</NavLink>
           </NavItem>
         </NavBar>
       </NavContainer>
