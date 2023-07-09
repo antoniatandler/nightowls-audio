549ba3187882bd49e974fb385709e81f40108d14
Antonia Tandler <q>
2023-07-09 06:52:36 +0200

    created rentail and events page

--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,54 @@
 import Heading from "@/components/Heading";
 import Navigation from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import styled from "styled-components";
+
+const EventContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: no-wrap;
+  justify-content: space-around;
+  position: absolute;
+  top: 20%;
+  width: 100%;
+  height: 500px;
+  text-align: center;
+`;
+
+const StrictlyTechnoSection = styled.section`
+  width: 50%;
+  margin: 15px;
+  border: 3px solid green;
+  flex-direction: column;
+`;
+
+const NightowlsGoaSection = styled.section`
+  width: 50%;
+  margin: 15px;
+  border: 3px solid green;
+  flex-direction: column;
+`;
+
+const EventsHeading = styled.h4`
+  color: white;
+  font-size: 30px;
+  margin-top: 70%;
+`;
 
 export default function Events() {
   return (
     <>
       <Heading />
       <Navigation />
+      <EventContainer>
+        <StrictlyTechnoSection>
+          <EventsHeading>STRICTLY TECHNO</EventsHeading>
+        </StrictlyTechnoSection>
+
+        <NightowlsGoaSection>
+          <EventsHeading>NACHTEULEN GOA</EventsHeading>
+        </NightowlsGoaSection>
+      </EventContainer>
       <Footer />
     </>
   );
--- pages/rentail.js
+++ pages/rentail.js
@@ -1,12 +1,58 @@
 import Footer from "@/components/Footer";
 import Heading from "@/components/Heading";
 import Navigation from "@/components/Navbar";
+import styled from "styled-components";
+
+const RentailContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  position: absolute;
+  top: 20%;
+  color: white;
+  width: 100%;
+  justify-content: space-around;
+`;
+
+const RentailSection = styled.section`
+  width: 25%;
+  border: 3px solid green;
+  text-align: center;
+  padding: 20px;
+  margin: 10px;
+  height: 450px;
+`;
+
+const RentailHeading = styled.h4`
+  font-size: 20px;
+  letter-spacing: 2px;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.5);
+  margin: 15px;
+`;
 
 export default function Rentail() {
   return (
     <>
       <Heading />
       <Navigation />
+      <RentailContainer>
+        <RentailSection>
+          <RentailHeading>LIGHTSHOW</RentailHeading>
+          <article>Füllhaltertext</article>
+        </RentailSection>
+        <RentailSection>
+          <RentailHeading>DECORATION</RentailHeading>
+          <article>Füllhaltertext</article>
+        </RentailSection>
+        <RentailSection>
+          <RentailHeading>EQUIPMENT</RentailHeading>
+          <article>Füllhaltertext</article>
+        </RentailSection>
+        <RentailSection>
+          <RentailHeading>EVENTSERVICE</RentailHeading>
+          <article>Füllhaltertext</article>
+        </RentailSection>
+      </RentailContainer>
       <Footer />
     </>
   );
